2c7f3f5b3dfa6a9ea8d2a49b28c55e6b3436c3f8
mjmandang0123 <mjmandang@tip.edu.ph>
2023-04-17 00:57:22 +0800

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,19 @@
+import { ChatBox } from "../components/chat-box";
+
+function Home() {
+  return (
+    <div className="wrapper">
+      <section>
+        <h1>ChillChat</h1>
+        <p>
+          
+        </p>
+      </section>
+      <section>
+        <ChatBox />
+      </section>
+    </div>
+  );
+}
+
+export default Home;
